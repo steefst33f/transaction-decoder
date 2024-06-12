0dd55f7edbc3297f6baea0647855a42510f203b1
steef <94022938+steefst33f@users.noreply.github.com>
2024-06-12 21:43:13 +0200

    Exercise 19: Replace unwraps with more proper error handling

--- src/lib.rs
+++ src/lib.rs
@@ -9,58 +9,59 @@ use transaction::{Input, Output, Transaction, Txid};
 mod amount;
 use amount::Amount;
 
-fn read_u32(bytes_slice: &mut &[u8]) -> u32 {
+fn read_u32(bytes_slice: &mut &[u8]) -> std::io::Result<u32> {
     // Read slice into a buffer
     let mut buffer = [0; 4];
-    bytes_slice.read(&mut buffer).unwrap();
-    u32::from_le_bytes(buffer)
+    bytes_slice.read(&mut buffer)?;
+    Ok(u32::from_le_bytes(buffer))
 }
 
-fn read_u64(bytes_slice: &mut &[u8]) -> u64 {
+fn read_u64(bytes_slice: &mut &[u8]) -> std::io::Result<u64> {
     let mut buffer = [0; 8];
-    bytes_slice.read(&mut buffer).unwrap();
-    u64::from_le_bytes(buffer)
+    bytes_slice.read(&mut buffer)?;
+    Ok(u64::from_le_bytes(buffer))
 }
 
-fn read_amount(bytes_slice: &mut &[u8]) -> Amount {
-    Amount::from_sat(read_u64(bytes_slice))
+fn read_amount(bytes_slice: &mut &[u8]) -> std::io::Result<Amount> {
+    let value = read_u64(bytes_slice)?;
+    Ok(Amount::from_sat(value))
 }
 
-fn read_script(bytes_slice: &mut &[u8]) -> String {
-    let script_size = read_compact_size_integer(bytes_slice) as usize;
+fn read_script(bytes_slice: &mut &[u8]) -> std::io::Result<String> {
+    let script_size = read_compact_size_integer(bytes_slice)? as usize;
     println!("script_size: {}", script_size);
     let mut buffer = vec![0_u8; script_size];
 
-    bytes_slice.read(&mut buffer).unwrap();
-    hex::encode(buffer)
+    bytes_slice.read(&mut buffer)?;
+    Ok(hex::encode(buffer))
 }
 
-fn read_txid(bytes_slice:&mut &[u8]) -> Txid {
+fn read_txid(bytes_slice:&mut &[u8]) -> std::io::Result<Txid> {
     let mut buffer = [0; 32];
-    _ = bytes_slice.read(&mut buffer);
-    Txid::from_bytes(buffer)
+    _ = bytes_slice.read(&mut buffer)?;
+    Ok(Txid::from_bytes(buffer))
 }
 
-pub fn read_compact_size_integer(bytes_slice: &mut &[u8]) -> u64 {
+pub fn read_compact_size_integer(bytes_slice: &mut &[u8]) -> std::io::Result<u64> {
     let mut compact_size = [0; 1];
-    bytes_slice.read(&mut compact_size).unwrap();
+    bytes_slice.read(&mut compact_size)?;
     
     match compact_size[0] {
-        0..=252 => compact_size[0] as u64,
+        0..=252 => Ok(compact_size[0] as u64),
         253 => {
             let mut buffer = [0; 2];
             bytes_slice.read(&mut buffer).unwrap();
-            u16::from_le_bytes(buffer) as u64
+            Ok(u16::from_le_bytes(buffer) as u64)
         },
         254 => {
             let mut buffer = [0; 4];
             bytes_slice.read(&mut buffer).unwrap();
-            u32::from_le_bytes(buffer) as u64
+            Ok(u32::from_le_bytes(buffer) as u64)
         },
         255 => {
             let mut buffer = [0; 8];
             bytes_slice.read(&mut buffer).unwrap();
-            u64::from_le_bytes(buffer) as u64
+            Ok(u64::from_le_bytes(buffer) as u64)
         }
     }
 }
@@ -80,25 +81,25 @@ pub fn run(raw_transaction_hex: String) -> Result<String, Box<dyn Error>> {
     let transaction_bytes = hex::decode(raw_transaction_hex).map_err(|e| format!("Hex decoding error: {}", e))?;
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let version = read_u32(&mut bytes_slice);
+    let version = read_u32(&mut bytes_slice)?;
     println!("Version: {}", version);
 
-    let input_length = read_compact_size_integer(&mut bytes_slice);
+    let input_length = read_compact_size_integer(&mut bytes_slice)?;
     println!("input_length: {}", input_length);
 
     let mut inputs = vec![];
 
     for input_number in 0..input_length {
-        let txid = read_txid(&mut bytes_slice);
+        let txid = read_txid(&mut bytes_slice)?;
         println!("txid[{}] = {:?}",input_number, txid);
 
-        let output_index = read_u32(&mut bytes_slice);
+        let output_index = read_u32(&mut bytes_slice)?;
         println!("output_index: {}", output_index);
 
-        let script = read_script(&mut bytes_slice);
+        let script = read_script(&mut bytes_slice)?;
         println!("unlocking_script: {:?}", script);
 
-        let sequence = read_u32(&mut bytes_slice);
+        let sequence = read_u32(&mut bytes_slice)?;
         println!("sequence: {}", sequence);
 
         inputs.push(Input {
@@ -109,13 +110,13 @@ pub fn run(raw_transaction_hex: String) -> Result<String, Box<dyn Error>> {
         });
     }
 
-    let output_count = read_compact_size_integer(&mut bytes_slice);
+    let output_count = read_compact_size_integer(&mut bytes_slice)?;
     println!("output_count: {}", output_count);
     let mut outputs: Vec<Output> = vec![];
 
     for _ in 0..output_count {
-        let amount = read_amount(&mut bytes_slice);
-        let output_script = read_script(&mut bytes_slice);
+        let amount = read_amount(&mut bytes_slice)?;
+        let output_script = read_script(&mut bytes_slice)?;
 
         outputs.push(Output {
             amount,
@@ -123,7 +124,7 @@ pub fn run(raw_transaction_hex: String) -> Result<String, Box<dyn Error>> {
         });
     }
 
-    let locktime = read_u32(&mut bytes_slice);
+    let locktime = read_u32(&mut bytes_slice)?;
     println!("locktime: {}", locktime);
 
     let txid = hash_transaction(&transaction_bytes);
@@ -148,28 +149,28 @@ mod unit_tests {
     #[test]
     fn test_read_compact_size_integer_one_byte() {
         let mut bytes = [1_u8].as_slice();
-        let length = read_compact_size_integer(&mut bytes);
+        let length = read_compact_size_integer(&mut bytes).unwrap();
         assert_eq!(length, 1_u64);
     }
 
     #[test]
     fn test_read_compact_size_integer_three_bytes() {
         let mut bytes = [253_u8, 0, 1].as_slice();
-        let length = read_compact_size_integer(&mut bytes);
+        let length = read_compact_size_integer(&mut bytes).unwrap();
         assert_eq!(length, 256_u64);
     }
 
     #[test]
     fn test_read_compact_size_integer_five_bytes() {
         let mut bytes = [254_u8, 0, 0, 0, 1].as_slice();
-        let length = read_compact_size_integer(&mut bytes);
+        let length = read_compact_size_integer(&mut bytes).unwrap();
         assert_eq!(length, 256_u64.pow(3));
     }
 
     #[test]
     fn test_read_compact_size_integer_nine_bytes() {
         let mut bytes = [255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
-        let length = read_compact_size_integer(&mut bytes);
+        let length = read_compact_size_integer(&mut bytes).unwrap();
         assert_eq!(length, 256_u64.pow(7));
     }
 
@@ -181,7 +182,7 @@ mod unit_tests {
         let hex = "fd204e";
         let decoded = hex::decode(hex).unwrap();
         let mut bytes = decoded.as_slice();
-        let length = read_compact_size_integer(&mut bytes);
+        let length = read_compact_size_integer(&mut bytes).unwrap();
         let expected_length = 20_000_u64;
         assert_eq!(length, expected_length);
     }
